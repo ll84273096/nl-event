24144c8439230868d9d04a1efbe0c972fdaa78ac
newtonliu <newtonliu@seismic.com>
2020-11-20 16:40:44 +0800

    refactor(*): improve Event and EventTarget

--- ts/event/Event.ts
+++ ts/event/Event.ts
@@ -1,41 +1,31 @@
 class Event {
     public readonly type: string | symbol;
-    // public target: unknown;
-    // public currentTarget: unknown;
-    private _target: unknown = null;
     private _currentTarget: unknown = null;
-    private _isFrozen: boolean = false;
+    private _target: unknown = null;
 
     constructor(type: string | symbol) {
         this.type = type;
-    }
-
-    public get target(): unknown {
-        return this._target;
-    }
-
-    public set target(target: unknown) {
-        if (!this._isFrozen) {
-            this._target = target;
-        }
     }
 
     public get currentTarget(): unknown {
         return this._currentTarget;
     }
 
-    public set currentTarget(currentTarget: unknown) {
-        if (!this._isFrozen) {
-            this._currentTarget = currentTarget;
-        }
+    public get target(): unknown {
+        return this._target;
     }
 
     public clone(): Event {
         return new Event(this.type);
     }
 
-    public freeze(): Event {
-        this._isFrozen = true;
+    public setCurrentTarget(currentTarget: unknown): Event {
+        this._currentTarget = currentTarget;
+        return this;
+    }
+
+    public setTarget(target: unknown): Event {
+        this._target = target;
         return this;
     }
 }
